cf14a6a516568cc57fe33c93150edb965359d756
Miguel Hernandez Giusti <mh@andsoluciones.com>
2025-08-25 16:42:33 -0500

    Refactor oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,18 +6,6 @@ return {
   -- Optional dependencies
   dependencies = {
     { "echasnovski/mini.icons", opts = {} },
-    { "benomahony/oil-git.nvim",
-      opts = {
-        highlights = {
-          OilGitAdded = { fg = "#a6e3a1" },     -- green
-          OilGitModified = { fg = "#f9e2af" },  -- yellow  
-          OilGitDeleted = { fg = "#f38ba8" },   -- red
-          OilGitRenamed = { fg = "#cba6f7" },   -- purple
-          OilGitUntracked = { fg = "#89b4fa" }, -- blue
-          OilGitIgnored = { fg = "#6c7086" },   -- gray
-        },
-      },
-    },
   },
   config = function()
     require("oil").setup({
@@ -34,9 +22,8 @@ return {
       view_options = {
         show_hidden = true,
       },
+      preview_split = "auto",
     })
   end,
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
 }
