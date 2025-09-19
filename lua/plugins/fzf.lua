fd23130e7beb35ed5d28e4dd8a16424f6979ae3b
Miguel Hernández Giusti <mh@andsoluciones.com>
2025-09-19 17:27:04 -0500

    Refactor fzflua configuration

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,9 +1,7 @@
 return {
   "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- or if using mini.icons/mini.nvim
-  -- dependencies = { "echasnovski/mini.icons" },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "echasnovski/mini.icons" },
   opts = {},
   config = function()
     local fzf_lua = require("fzf-lua")
@@ -42,15 +40,15 @@ return {
           -- `file_edit_or_qf` opens a single selection or sends multiple selection to quickfix
           -- replace `enter` with `file_edit` to open all files/bufs whether single or multiple
           -- replace `enter` with `file_switch_or_edit` to attempt a switch in current tab first
-          ["enter"]       = fzf_lua.actions.file_edit_or_qf,
-          ["ctrl-s"]      = fzf_lua.actions.file_split,
-          ["ctrl-v"]      = fzf_lua.actions.file_vsplit,
-          ["ctrl-t"]      = fzf_lua.actions.file_tabedit,
-          ["alt-q"]       = fzf_lua.actions.file_sel_to_qf,
-          ["alt-Q"]       = fzf_lua.actions.file_sel_to_ll,
-          ["alt-i"]       = fzf_lua.actions.toggle_ignore,
-          ["alt-h"]       = fzf_lua.actions.toggle_hidden,
-          ["alt-f"]       = fzf_lua.actions.toggle_follow,
+          ["enter"]  = fzf_lua.actions.file_edit_or_qf,
+          ["ctrl-s"] = fzf_lua.actions.file_split,
+          ["ctrl-v"] = fzf_lua.actions.file_vsplit,
+          ["ctrl-t"] = fzf_lua.actions.file_tabedit,
+          ["alt-q"]  = fzf_lua.actions.file_sel_to_qf,
+          ["alt-Q"]  = fzf_lua.actions.file_sel_to_ll,
+          ["alt-i"]  = fzf_lua.actions.toggle_ignore,
+          ["alt-h"]  = fzf_lua.actions.toggle_hidden,
+          ["alt-f"]  = fzf_lua.actions.toggle_follow,
         },
       }
 
